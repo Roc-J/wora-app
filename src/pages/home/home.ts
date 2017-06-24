d651ebdd357a93325732e171d048573b009dac82
Roc-J <646481338@qq.com>
2017-06-24 20:38:52 +0800

    work

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,13 +14,6 @@ import { DetailPage } from '../detail/detail';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  // We proxied rssUrl to http://bbs.cloud.icybee.cn/rss/
-
-  // for `ionic serve`
-  // private readonly rssUrl = '/rss/board-Picture'
-
-  // for `electron .`
-  // private readonly rssUrl = 'http://bbs.cloud.icybee.cn/rss/board-Picture'
 
   films: Observable<any>;
 
@@ -34,40 +27,13 @@ export class HomePage {
   }
 
   ngOnInit() {
-    // this.initBuptBbsRss()
 
-    this.films = this.http.get('http://swapi.co/api/films')
+    this.films = this.http.get('http://japi.juhe.cn/health_knowledge/categoryList?=&key=f00733530cda3adc4eae151152938946')
                           .map(res => res.json());
   }
 
   openDetails(film) {
     this.navCtrl.push(DetailPage, {film: film});
   }
-  // @angular/http
-  // initBuptBbsRss() {
-  //   this.http.get(this.rssUrl)
-  //                   .map(res => res.text()) //toJson(res.text()))
-  //                   .subscribe(
-  //                     data => {
-
-  //                       const fixedData = data.replace(
-  //                                                 /href="\\/g,
-  //                                                 'href="#',
-  //                                               )
-  //                                               .replace(
-  //                                                 /src="\//g,
-  //                                                 'src="http://bbs.cloud.icybee.cn/',
-  //                                               )
-  //                       parseString(fixedData, (err, result) => {
-  //                         this.posts = result.rss.channel[0].item
-  //                         console.log(this.posts)
-  //                       })
-
-  //                     },
-  //                     err => {
-  //                       console.log("Oops!", err);
-  //                     }
-  //                   )
-  // }
 
 }
